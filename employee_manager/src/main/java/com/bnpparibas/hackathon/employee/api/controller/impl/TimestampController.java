d9c94b5cae1aab8b9e6e574220fe622e7a7d00a2
Jean <jean.coppieters@hotmail.com>
2020-03-14 11:15:55 +0000

    Fixing endpoint position

--- employee_manager/src/main/java/com/bnpparibas/hackathon/employee/api/controller/impl/TimestampController.java
+++ employee_manager/src/main/java/com/bnpparibas/hackathon/employee/api/controller/impl/TimestampController.java
@@ -1,6 +1,6 @@
-package com.bnpparibas.hackathon.findmyspot.api.controller.impl;
+package com.bnpparibas.hackathon.employee.api.controller.impl;
 
-import com.bnpparibas.hackathon.findmyspot.api.controller.TimestampControllerAPI;
+import com.bnpparibas.hackathon.employee.api.controller.TimestampControllerAPI;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
