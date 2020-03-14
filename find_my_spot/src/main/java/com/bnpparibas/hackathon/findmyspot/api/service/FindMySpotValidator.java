8b77d110400b0555edd54597550d5fa20ec9761f
bnp-hackathon-api-teamd <62019870+bnp-hackathon-api-teamd@users.noreply.github.com>
2020-03-14 14:16:40 +0000

    Ricardo - validacao employee

--- find_my_spot/src/main/java/com/bnpparibas/hackathon/findmyspot/api/service/FindMySpotValidator.java
+++ find_my_spot/src/main/java/com/bnpparibas/hackathon/findmyspot/api/service/FindMySpotValidator.java
@@ -1,15 +1,21 @@
 package com.bnpparibas.hackathon.findmyspot.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.bnpparibas.hackathon.employee.api.model.Employee;
 
 @Service
 public class FindMySpotValidator {
-
+	
     public boolean existsEmployee(Long employeeId) {
-        throw new RuntimeException("existsEmployee");
+    	ResponseEntity<Employee> employee = template.getForEntity("http://localhost:8080/employee/api/v1/employees/"+employeeId, Employee.class);
+    	if(employee != null) {
+    		return true;	
+    	}
+    	return false;
     }
 
     public boolean employeeIsInSpot(Long employeeId) {
