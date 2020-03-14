5bea4d315b02441aa543a958e27f6cfb81fdacdb
Jean <bnp.hackathon.api.teamd@outlook.pt>
2020-03-14 13:10:42 +0000

    adding endpoint

---
+++ find_my_spot/src/main/java/com/bnpparibas/hackathon/findmyspot/api/service/FindMySpotService.java
@@ -0,0 +1,7 @@
+package com.bnpparibas.hackathon.findmyspot.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FindMySpotService {
+}
---
+++ find_my_spot/src/main/java/com/bnpparibas/hackathon/findmyspot/api/service/FindMySpotValidator.java
@@ -0,0 +1,12 @@
+package com.bnpparibas.hackathon.findmyspot.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FindMySpotValidator {
+    public boolean existsEmployee(Long employeeId) {
+    }
+
+    public boolean employeeIsInSpot(Long employeeId) {
+    }
+}
